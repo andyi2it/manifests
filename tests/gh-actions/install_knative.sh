9fac708a648f7ddb7b588e71f1991c11f3d30d12
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-02-01 00:38:11 +0530

    Revert knative installation changes
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_knative.sh
+++ tests/gh-actions/install_knative.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 echo "Installing KNative ..."
-
+set +e
+kustomize build common/knative/knative-serving/base | kubectl apply -f -
+set -e
 kustomize build common/knative/knative-serving/base | kubectl apply -f -
 
 kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
