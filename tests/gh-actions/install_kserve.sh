58dd2cfd53f7a5373308b1fdbe4f1b86763c5819
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2023-02-07 11:35:04 +0530

    Add debug info
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- tests/gh-actions/install_kserve.sh
+++ tests/gh-actions/install_kserve.sh
@@ -8,6 +8,9 @@ set -e
 echo "Waiting for crd/clusterservingruntimes.serving.kserve.io to be available ..."
 kubectl wait --for condition=established --timeout=30s crd/clusterservingruntimes.serving.kserve.io
 kustomize build kserve | kubectl apply -f -
+echo "Installing models webapp"
 kustomize build models-web-app/overlays/kubeflow | kubectl apply -f -
 kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 180s
+kubectl get po -A
+kubectl get deploy -A
 kubectl patch cm config-domain --patch '{"data":{"example.com":""}}' -n knative-serving
